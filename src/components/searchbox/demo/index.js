52474e1baaa20d498512ef3687dc177751b0aeab
jigargandhi <jigarr.gandhi@gmail.com>
2015-12-23 15:02:14 +0530

    adding searchbox directive
    closes issue #39
    merges pr #81

---
+++ src/components/searchbox/demo/index.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+var demoApp = angular.module('demoApp', [
+  'officeuifabric.core',
+  'officeuifabric.components.searchbox'
+]);
+
+demoApp.controller('demoController', [
+  '$scope', demoController]);
+
+function demoController($scope) {
+    $scope.placeholder = "Placeholder text";
+    $scope.value= "search keyword"
+}
--- src/core/components.ts
+++ src/core/components.ts
@@ -4,6 +4,7 @@ import * as ng from 'angular';
 import * as contextualMenuModule from '../components/contextualmenu/contextualMenu';
 import * as dropdownModule from '../components/dropdown/dropdownDirective';
 import * as iconModule from '../components/icon/iconDirective';
+import * as searchboxModule from '../components/searchbox/searchboxDirective';
 import * as spinnerModule from '../components/spinner/spinnerDirective';
 import * as tableModule from '../components/table/tableDirective';
 import * as textFieldModule from '../components/textfield/textFieldDirective';
@@ -19,11 +20,12 @@ import * as toggleModule from '../components/toggle/toggleDirective';
  *
  */
 export var module: ng.IModule = ng.module('officeuifabric.components', [
-    contextualMenuModule.module.name,
-    dropdownModule.module.name,
-    iconModule.module.name,
-    spinnerModule.module.name,
-    tableModule.module.name,
-    textFieldModule.module.name,
-    toggleModule.module.name
+  contextualMenuModule.module.name,
+  dropdownModule.module.name,
+  iconModule.module.name,
+  searchboxModule.module.name,
+  spinnerModule.module.name,
+  tableModule.module.name,
+  textFieldModule.module.name,
+  toggleModule.module.name
 ]);
