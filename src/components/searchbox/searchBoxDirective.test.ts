2e7a29bb5597ca8cd6d01aa2b3412c42681ab860
jigargandhi <jigarr.gandhi@gmail.com>
2015-12-23 15:02:14 +0530

    Adding searchbox directive files

---
+++ src/components/searchbox/searchBoxDirective.test.ts
@@ -0,0 +1,49 @@
+﻿/// <reference path="../../../typings/angularjs/angular-mocks.d.ts" />
+/// <reference path="../../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../../typings/karma/karma.d.ts" />
+/// <reference path="../../../typings/jasmine/jasmine.d.ts" />
+describe("searchBoxDirective", () => {
+    beforeEach(() => {
+        angular.mock.module('fabric.ui.components.searchbox');
+    });
+
+    afterEach(() => {
+        // myfunc.reset();
+    });
+
+    it("should have unique ids", inject(($compile, $rootScope) => {
+        var $scope = $rootScope.$new();
+        var textBox1 = $compile('<uif-Searchbox value="textBoxValue"></uif-Searchbox>')($scope);
+        $scope.$apply();
+
+        var textField1 = $(textBox1[0]).find('.ms-SearchBox-field');
+
+        var textBox2 = $compile('<uif-Searchbox value="textBoxValue"></uif-Searchbox>')($scope);
+        $scope.$apply();
+        var textField2 = $(textBox2[0]).find('.ms-SearchBox-field');
+        expect(textField1[0].id === textField2[0].id).toBe(false);
+
+    }));
+    it("should be able to set value", inject(($compile, $rootScope) => {
+        var $scope = $rootScope.$new();
+        $scope.textBoxValue = "Test 1";
+        var textBox = $compile('<uif-Searchbox value="textBoxValue"></uif-Searchbox>')($scope);
+        $scope.$apply();
+        
+        var textField = $(textBox[0]).find('.ms-SearchBox-field');
+        expect(textField.length).toBe(1);
+        expect(textField.val()).toBe('Test 1');
+
+        $scope.textBoxValue = "Test 2";
+        $scope.$apply();
+        expect(textField.val()).toBe('Test 2');
+
+        textField.val('Test 3');
+        textField.trigger('input');
+        
+        expect(textField.val()).toBe('Test 3', 'Update textbox');
+        
+        // todo: In the browser this works fine. Not sure why not here :(
+        // expect($scope.textBoxValue).toBe('Test 3', 'Scope update parent');
+    }));
+});
---
+++ src/components/searchbox/searchBoxDirective.ts
@@ -0,0 +1,83 @@
+﻿
+
+
+class SearchBoxDirective implements ng.IDirective {
+
+    template= '<div class="ms-SearchBox">' +
+    '<input class="ms-SearchBox-field" ng-model="value" id="{{\'searchBox_\'+uniqueId}}" >' +
+    '<label class="ms-SearchBox-label" for="{{\'searchBox_\'+uniqueId}}"><i class="ms-SearchBox-icon ms-Icon ms-Icon--search" ></i>{{search}}</label>' +
+    '<button class="ms-SearchBox-closeButton"  type="button"><i class="ms-Icon ms-Icon--x"></i></button>' +
+    '</div>';
+    constructor() {
+    }
+    uniqueId = 1;
+    scope = {
+        value: "=value",
+        search: "=",
+        close: "&close"
+    }
+   
+    link(scope, elem: ng.IAugmentedJQuery, attrs: ng.IAttributes) {
+
+        if (!this.uniqueId)
+            this.uniqueId = 1;
+        scope.uniqueId = this.uniqueId++;
+        var cancel = false;
+        var focus = false;
+        elem.find('.ms-SearchBox-field').on('focus', function () {
+            /** Hide the label on focus. */
+            $(this).siblings('.ms-SearchBox-label').hide();
+            // Show cancel button by adding is-active class
+            $(this).parent('.ms-SearchBox').addClass('is-active');
+            focus = true;
+        });
+        elem.find('.ms-SearchBox-closeButton').on('mousedown', function () {
+            cancel = true;
+        });
+        elem.find('.ms-SearchBox-field').on('blur', function () {
+            var that = this;
+            if (cancel) {
+                scope.$apply(function () {
+
+                    scope.value = "";
+                });
+                $(this).siblings('.ms-SearchBox-label').show();
+              
+
+            }
+			
+			  $(this).parent('.ms-SearchBox').removeClass('is-active');
+            if (scope.value.length === 0) {
+                $(this).siblings('.ms-SearchBox-label').show();
+            }
+
+            cancel = false;
+            focus = false;
+        });
+
+        scope.$watch("value", function (val) {
+
+            if (!focus) {
+                if (val&&val != "") {
+                    elem.find('.ms-SearchBox-label').hide();
+                }
+                else {
+                    elem.find('.ms-SearchBox-label').show();
+                }
+            }
+        });
+
+
+
+    }
+
+    static factory(): ng.IDirectiveFactory {
+        const directive = () => new SearchBoxDirective();
+
+        return directive;
+    }
+}
+
+angular.module("fabric.ui.components.searchbox", ['fabric.ui.components'])
+    .directive("uifSearchbox", SearchBoxDirective.factory()); 
+
