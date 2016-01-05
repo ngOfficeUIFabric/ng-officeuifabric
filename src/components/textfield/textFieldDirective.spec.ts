ba03b75c274c26876194569febded4adc368121c
rolandoldengarm <r_oldengarm@hotmail.com>
2016-01-06 09:35:30 +1300

    Updated textfield
    Documentation, vet issues, examples

--- src/components/textfield/textFieldDirective.spec.ts
+++ src/components/textfield/textFieldDirective.spec.ts
@@ -1,4 +1,4 @@
-﻿function getMainDiv(rootControl: JQuery) {
+﻿function getMainDiv(rootControl: JQuery): JQuery {
     return rootControl.find('div');
 }
 
@@ -12,40 +12,41 @@ describe('textFieldDirective', () => {
         $scope.textBoxValue = 'Test 1';
         let textBox: JQuery = $compile('<uif-textfield ng-model="textBoxValue"></uif-textfield>')($scope);
         $scope.$apply();
-        var mainDiv = getMainDiv(textBox);
-        
+        let mainDiv: JQuery = getMainDiv(textBox);
+
         let input: JQuery = mainDiv.find('input');
         expect(input.val()).toBe('Test 1');
-        
+
         $scope.textBoxValue = 'Test 2';
         $scope.$apply();
         expect(input.val()).toBe('Test 2');
     }));
-    
+
     it('should be able to set label and description', inject(($compile: Function, $rootScope: ng.IRootScopeService) => {
         let $scope: any = $rootScope.$new();
         let textBox: JQuery = $compile('<uif-textfield label="Label contents" description="Description contents"></uif-textfield>')($scope);
         $scope.$apply();
-        
+
         let label: JQuery = getMainDiv(textBox).find('label.ms-Label');
         expect(label.html()).toBe('Label contents');
-        
+
         let description: JQuery = textBox.find('span.ms-TextField-description');
         expect(description.html()).toBe('Description contents');
     }));
-    
+
     it('should be able to set underlined', inject(($compile: Function, $rootScope: ng.IRootScopeService) => {
         let $scope: any = $rootScope.$new();
         let textBox: JQuery = $compile('<uif-textfield underlined></uif-textfield>')($scope);
         $scope.$apply();
+
         let div: JQuery = getMainDiv(textBox);
-        
-        expect(div.hasClass("ms-TextField--underlined")).toBe(true, "textfield should have ms-textfield--underlined");
+        expect(div.hasClass('ms-TextField--underlined')).toBe(true, 'textfield should have ms-textfield--underlined');
     }));
     it('should be able to set placeholder', inject(($compile: Function, $rootScope: ng.IRootScopeService) => {
         let $scope: any = $rootScope.$new();
         let textBox: JQuery = $compile('<uif-textfield placeholder="Placeholder contents"></uif-textfield>')($scope);
         $scope.$apply();
+
         let label: JQuery = textBox.find('div.ms-TextField--placeholder label.ms-Label');
         expect(label.html()).toBe('Placeholder contents');
     }));
--- src/components/textfield/textFieldDirective.ts
+++ src/components/textfield/textFieldDirective.ts
@@ -25,19 +25,31 @@ import * as ng from 'angular';
  *       />
  */
 
+export interface ITextFieldScope extends ng.IScope {
+    labelShown: boolean;
+    underlined: boolean;
+    inputFocus: (ev: any) => void;
+    inputClick: (ev: any) => void;
+    inputBlur: (ev: any) => void;
+    isActive: boolean;
+    placeholder: string;
+}
+
 export class TextFieldDirective implements ng.IDirective {
-    public template: string = 
-    '<div ng-class="{\'is-active\': isActive, \'ms-TextField\': true, \'ms-TextField--underlined\': underlined, \'ms-TextField--placeholder\': placeholder}">' +
-                                '<label ng-show="labelShown" class="ms-Label">{{label || placeholder}}</label>' +
-                                '<input ng-blur="inputBlur()" ng-focus="inputFocus()" ng-click="inputClick()" class="ms-TextField-field" />' +
-                                '<span class="ms-TextField-description">{{description}}</span>' +
-                                '</div>';
+    public template: string =
+        '<div ng-class="{\'is-active\': isActive, \'ms-TextField\': true, ' +
+            '\'ms-TextField--underlined\': underlined, \'ms-TextField--placeholder\': placeholder}">' +
+            '<label ng-show="labelShown" class="ms-Label">{{label || placeholder}}</label>' +
+            '<input ng-blur="inputBlur()" ng-focus="inputFocus()" ng-click="inputClick()" class="ms-TextField-field" />' +
+            '<span class="ms-TextField-description">{{description}}</span>' +
+        '</div>';
     public scope: {} = {
-        label: "@",
-        description: "@",
-        placeholder: "@"
-    }              
-    public require: string = '?ngModel';            
+        description: '@',
+        label: '@',
+        placeholder: '@'
+    };
+
+    public require: string = '?ngModel';
     public restrict: string = 'E';
     public static factory(): ng.IDirectiveFactory {
         const directive: ng.IDirectiveFactory = () => new TextFieldDirective();
@@ -45,37 +57,35 @@ export class TextFieldDirective implements ng.IDirective {
         return directive;
     }
 
-    public link(scope: ng.IScope, instanceElement: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController): void {
-        scope["labelShown"] = true;
-        scope["underlined"] = "underlined" in attrs;
-        scope["inputFocus"] = function(ev) {
-            if (scope["underlined"]) {
-                scope["isActive"] = true;
+    public link(scope: ITextFieldScope, instanceElement: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController): void {
+        scope.labelShown = true;
+        scope.underlined = 'underlined' in attrs;
+        scope.inputFocus = function(ev: any): void {
+            if (scope.underlined) {
+                scope.isActive = true;
             }
-        }
-        scope["inputClick"] = function(ev) {
-            if (scope["placeholder"]) {
-                scope["labelShown"] = false;
+        };
+        scope.inputClick = function(ev: any): void {
+            if (scope.placeholder) {
+                scope.labelShown = false;
             }
-        }
-        scope["inputBlur"] = function (ev) {
-            var input = instanceElement.find('input');
-            if (scope["placeholder"] && input.val().length === 0) {
-                scope["labelShown"] = true;
+        };
+        scope.inputBlur = function (ev: any): void {
+            let input: JQuery = instanceElement.find('input');
+            if (scope.placeholder && input.val().length === 0) {
+                scope.labelShown = true;
             }
-            if (scope["underlined"]) {
-                scope["isActive"] = false;
+            if (scope.underlined) {
+                scope.isActive = false;
             }
-        }
+        };
         if (ngModel != null) {
-            ngModel.$render = function() {
-                let input = instanceElement.find("input");       
+            ngModel.$render = function(): void {
+                let input: JQuery = instanceElement.find('input');
                 input.val(ngModel.$modelValue);
                 input.change();
             };
         }
-        
-        
     }
 }
 
