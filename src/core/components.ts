33b8485d1a6946e97f7986805ad4545dfb813b36
Waldek Mastykarz <waldek@mastykarz.nl>
2016-01-08 16:17:14 +0100

    p1 for table directive #36

--- src/core/components.ts
+++ src/core/components.ts
@@ -4,6 +4,7 @@ import * as ng from 'angular';
 import * as iconModule from '../components/icon/iconDirective';
 import * as textFieldModule from '../components/textfield/textFieldDirective';
 import * as spinnerModule from '../components/spinner/spinnerDirective';
+import * as tableModule from '../components/table/tableDirective';
 
 
 /**
@@ -18,5 +19,6 @@ import * as spinnerModule from '../components/spinner/spinnerDirective';
 export var module: ng.IModule = ng.module('officeuifabric.components', [
     iconModule.module.name,
     textFieldModule.module.name,
-    spinnerModule.module.name
+    spinnerModule.module.name,
+    tableModule.module.name
   ]);
