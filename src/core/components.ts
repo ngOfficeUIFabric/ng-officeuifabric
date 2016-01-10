a0ace3cccc4fe077466190de8b501139be7a5f6f
rolandoldengarm <r_oldengarm@hotmail.com>
2016-01-11 09:01:35 +1300

    Added toggle to core components.ts

--- src/core/components.ts
+++ src/core/components.ts
@@ -4,6 +4,7 @@ import * as ng from 'angular';
 import * as iconModule from '../components/icon/iconDirective';
 import * as textFieldModule from '../components/textfield/textFieldDirective';
 import * as spinnerModule from '../components/spinner/spinnerDirective';
+import * as toggleModule from '../components/toggle/toggleDirective';
 
 
 /**
@@ -18,5 +19,6 @@ import * as spinnerModule from '../components/spinner/spinnerDirective';
 export var module: ng.IModule = ng.module('officeuifabric.components', [
     iconModule.module.name,
     textFieldModule.module.name,
-    spinnerModule.module.name
+    spinnerModule.module.name,
+    toggleModule.module.name
   ]);
