90915b62bbef4d722595b93f12d28e604507b2d8
rolandoldengarm <r_oldengarm@hotmail.com>
2016-01-11 08:24:46 +1300

    Toggle Directive
    Initial check-in

---
+++ src/components/toggle/demo/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var demoApp = angular.module('demoApp', [
+  'officeuifabric.core',
+  'officeuifabric.components.toggle'
+]);
+
+demoApp.controller('demoController', [
+  '$scope', demoController]);
+
+function demoController($scope) {
+    
+}
