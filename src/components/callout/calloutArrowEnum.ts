72834f83068f3104a1932af0f87dfdcb8620dcf1
Jerzy Czopek <jerzy.czopek@outlook.com>
2016-01-11 22:34:52 +0100

    feat(callout): add new directive
    Add new directive `uif-callout`.
    
    Closes #15. Closes #119.

---
+++ src/components/callout/calloutArrowEnum.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Enum for callout arrow direction. It is intended to use as string as `uif-arrow` attribute value on `uif-callout` directive.
+ *
+ * @readonly
+ * @enum {string}
+ * @usage
+ *
+ * This is used to generate the string that you pass into the <uif-callout /> directive. Specifically, the string is passed
+ * to the `uif-arrow` attribute. To evaluate the enum value as a string:
+ *
+ * let arrow: string = CalloutArrow[CalloutArrow.left];
+ */
+export enum CalloutArrow {
+  left,
+  right,
+  top,
+  bottom
+}
