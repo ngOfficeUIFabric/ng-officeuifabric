0f8ee381dd884447d4c5fae400f4b4a86cf4ce6c
rolandoldengarm <r_oldengarm@hotmail.com>
2016-01-12 17:26:46 +1300

    Toggle fix demo including unique ID
    Demo scripts were missing. Found out that unique ID generation was not
    working due to "use strict"
    implemented a better approach.

---
+++ src/components/toggle/demo/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var demoApp = angular.module('demoApp', [
+  'officeuifabric.core',
+  'officeuifabric.components.toggle'
+]);
+
+demoApp.controller('demoController', [
+  '$scope', demoController]);
+
+function demoController($scope) {   
+}
