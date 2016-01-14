f740c39741219d8bd949857b309b78e1f65d1e41
Andrew Connell <me@andrewconnell.com>
2016-01-14 07:03:48 -0500

    added demo showing error in console using enum

--- src/components/icon/iconDirective.ts
+++ src/components/icon/iconDirective.ts
@@ -1,6 +1,21 @@
 'use strict';
 
 import * as ng from 'angular';
+import {IconEnum} from './iconEnum';
+
+/**
+ * @ngdoc interface
+ * @name IIconScope
+ * @module officeuifabric.components.contextualmenu
+ * 
+ * @description
+ * This is the scope used by the `<uif-icon />` directive.
+ * 
+ * @property {string} uifType - Icon to display. Possible types are defined in {@link IconEnum}.
+ */
+export interface IIconScope extends ng.IScope {
+  uifType: string;
+}
 
 /**
  * @ngdoc directive
@@ -31,6 +46,16 @@ export class IconDirective implements ng.IDirective {
     const directive: ng.IDirectiveFactory = () => new IconDirective();
     return directive;
   }
+
+  public link(scope: IIconScope, instanceElement: ng.IAugmentedJQuery, attrs: ng.IAttributes, ngModel: ng.INgModelController): void {
+    // verify a valid icon was passed in
+    if (IconEnum[scope.uifType] === undefined) {
+      console.error('Error [ngOfficeUiFabric] officeuifabric.components.icon - Unsupported icon specified: ' +
+                    'The icon specified (\'' + scope.uifType + '\') is not supported by the Office UI Fabric. ' +
+                    'Supported options are listed here: ' +
+                    'https://github.com/ngOfficeUIFabric/ng-officeuifabric/blob/master/src/components/icon/iconEnum.ts');
+    }
+  };
 }
 
 /**
@@ -42,6 +67,6 @@ export class IconDirective implements ng.IDirective {
  * 
  */
 export var module: ng.IModule = ng.module('officeuifabric.components.icon', [
-    'officeuifabric.components'
-  ])
+  'officeuifabric.components'
+])
   .directive('uifIcon', IconDirective.factory());
