62b89948db2046b6b26eff196bc6ab68bc2a0efe
John Meilleur <john@meilleur.ca>
2016-01-16 09:38:44 -0700

    overlay cleanup and additional tests

--- src/components/overlay/overlayDirective.ts
+++ src/components/overlay/overlayDirective.ts
@@ -5,28 +5,28 @@ import * as ng from 'angular';
 /**
  * @ngdoc enum
  * @name OverlayMode
- * 
- * @description 
+ *
+ * @description
  * ???
- * 
+ *
  */
 export enum OverlayMode {
-  light,
-  dark
+    light,
+    dark
 }
 
 /**
  * @ngdoc interface
  * @name IIconScope
  * @module officeuifabric.components.contextualmenu
- * 
+ *
  * @description
  * This is the scope used by the `<uif-icon />` directive.
- * 
+ *
  * @property {string} uifType - Icon to display. Possible types are defined in {@link IconEnum}.
  */
 export interface IOverlayScope extends ng.IScope {
-  uifMode: OverlayMode;
+    uifMode: OverlayMode;
 }
 
 /**
@@ -37,71 +37,72 @@ export interface IOverlayScope extends ng.IScope {
  * Used to more easily inject the Angular $log service into the directive.
  */
 class OverlayController {
-  public static $inject: string[] = ['$log'];
-  constructor(public log: ng.ILogService) {
-  }
+    public static $inject: string[] = ['$log'];
+    constructor(public log: ng.ILogService) {
+    }
 }
 
 /**
  * @ngdoc directive
  * @name uifOverlay
  * @module officeuifabric.components.overlay
- * 
+ *
  * @restrict E
- * 
- * @description 
+ *
+ * @description
  * `<uif-overlay>` is an overlay directive.
- * 
+ *
  * @see {link http://dev.office.com/fabric/styles}
- * 
+ *
  * @usage
- * 
+ *
  * <uif-overlay>html markup goes here</uif-overlay>
  * <uif-overlay uif-overlay-dark="true">html markup goes here</>
  */
 export class OverlayDirective implements ng.IDirective {
 
-  public restrict: string = 'E';
-  public template: string = '<div class="ms-Overlay" ng-class="{\'ms-Overlay--dark\': uifMode == \'dark\'}" ng-transclude></div>';
-  public scope: {} = {
-    uifMode: '@'
-  };
-  public transclude: Boolean = true;
-  public static log: ng.ILogService;
-  
-  public constructor(public log: ng.ILogService) {
-      OverlayDirective.log = log;
-  }
-  
-  public static factory(): ng.IDirectiveFactory {
-    const directive: ng.IDirectiveFactory = (log: ng.ILogService) => new OverlayDirective(log);
-    directive.$inject = ["$log"];
-    return directive;
-  }
-  
-  public link(scope: IOverlayScope): void {
-    // add watcher
-    
-    scope.$watch('uifMode', (newValue: string, oldValue: string) => {
-      // verify a valid overlay mode was passed in
-      if (OverlayMode[newValue] === undefined) {
-        OverlayDirective.log.error('Error [ngOfficeUiFabric] officeuifabric.components.overlay - Unsupported overlay mode: ' +
-          'The overlay mode (\'' + scope.uifMode + '\') is not supported by the Office UI Fabric. ' +
-          'Supported options are "light" and "dark"');
-      }
-    });
-  };
+    public static log: ng.ILogService;
+
+    public restrict: string = 'E';
+    public template: string = '<div class="ms-Overlay" ng-class="{\'ms-Overlay--dark\': uifMode == \'dark\'}" ng-transclude></div>';
+    public scope: {} = {
+        uifMode: '@'
+    };
+    public transclude: Boolean = true;
+
+    public constructor(public log: ng.ILogService) {
+        OverlayDirective.log = log;
+    }
+
+    public static factory(): ng.IDirectiveFactory {
+        const directive: ng.IDirectiveFactory = (log: ng.ILogService) => new OverlayDirective(log);
+        directive.$inject = ['$log'];
+        return directive;
+    }
+
+    public link(scope: IOverlayScope): void {
+
+        scope.$watch('uifMode', (newValue: string, oldValue: string) => {
+
+            // verify a valid overlay mode was passed in
+            if (OverlayMode[newValue] === undefined) {
+                OverlayDirective.log.error('Error [ngOfficeUiFabric] officeuifabric.components.overlay - Unsupported overlay mode: ' +
+                    'The overlay mode (\'' + scope.uifMode + '\') is not supported by the Office UI Fabric. ' +
+                    'Supported options are \'light\' and \'dark\'');
+            }
+        });
+    };
 }
 
 /**
  * @ngdoc module
  * @name officeuifabric.components.overlay
- * 
- * @description 
+ *
+ * @description
  * Overlay
- * 
+ *
  */
 export var module: ng.IModule = ng.module('officeuifabric.components.overlay', [
     'officeuifabric.components'
-  ])
-  .directive('uifOverlay', OverlayDirective.factory());
+])
+    .directive('uifOverlay', OverlayDirective.factory());
