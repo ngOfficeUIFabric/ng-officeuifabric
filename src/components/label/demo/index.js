1ce5c1bdabacca1928086f6f4993f97030d2111f
Jerzy Czopek <jerzy.czopek@outlook.com>
2016-02-01 20:39:53 +0100

    feat(label): add new directive
    Add new directive `uif-label`.

---
+++ src/components/label/demo/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var demoApp = angular.module('demoApp', [
+  'officeuifabric.core',
+  'officeuifabric.components.label'
+]);
+
+// demoApp.controller('labelDemoController',['$scope', labelDemoController]);
+//
+// function labelDemoController($scope){
+//
+// }
