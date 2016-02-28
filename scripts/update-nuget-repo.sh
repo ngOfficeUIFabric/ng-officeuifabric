927436704590673b07d456cdc8f33c973bddbdb7
Andrew Connell <me@andrewconnell.com>
2016-02-28 09:32:12 -0500

    chore(): add release script for nuget
    Add scripts to handle updating and pushing a new release for the NuGet repo
    
    References #9

---
+++ scripts/update-nuget-repo.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# NOTE: DO NOT RUN THIS SCRIPT UNLESS CREATING A RELEASE!!!!!!!!
+#   This should only be done by the project owner(s). If it is run by mistake, it will
+#   require cleanup in the main repos resulting in rewriting history & likey screwingup
+#   most forks.
+#
+# This script is only used when creating a new release. It expects the following:
+#   1. to be run from the `master` branch
+#   2. to be run from the root of the repo
+#   3. all updates to be included in the release that need to be applied
+#       have been applied to local `master` including updating package.json to
+#       the desired release version, the correct angular, office ui fabric & other
+#       dependencies & the changelog.md has been updated
+#
+# The script will do the following things:
+#   1. clone a copy of nuget-ngofficeuifabric to a temp location
+#   2. copy the compiled library & changelog to the cloned nuget-ngofficeuifabric
+#   3. update the nuget-ngofficeuifabric repo with the following changes by calling
+#       update-nuget-versions.js:
+#     3.1. update ng-office-ui-fabric.nuspec with current library dependencies
+#     3.2. update ng-office-ui-fabric.nuspec with current library version
+
+ARG_DEFS=(
+  "--version=(.*)"
+  "--src=(.*)"
+)
+
+function init {
+  # setup paths
+  SRC_PATH=$SRC
+  PKG_PATH="$(mktemp -d)"
+  # setup globals
+  REPO_URL="https://github.com/ngOfficeUIFabric/nuget-ngofficeuifabric"
+}
+
+function run {
+  # start from root ng-officeuifabric library
+  cd $SRC_PATH
+
+
+  # clone packaging repo
+  echo ".. clone packaging repo"
+  echo "..>>> $PKG_PATH"
+  git clone $REPO_URL $PKG_PATH --depth=2
+
+
+  # copy built library & changelog
+  echo ".. copying built library & changelog"
+  cp -Rf dist/*.js $PKG_PATH/src/content/Scripts
+  cp -Rf changelog.md $PKG_PATH/changelog.md
+
+
+  # update versions & dependencies in ng-office-ui-fabric.nuspec
+  echo ".. updating versions & dependencies in ng-office-ui-fabric.nuspec"
+  node scripts/update-nuget-versions.js --src=$PWD --pkg=$PKG_PATH
+
+  # update packaging repo
+  echo ".. updating packaging repo nuget-ngofficeuifabric"
+  cd $PKG_PATH
+
+  echo ".. .. adding & commiting changes to package repo"
+  git add -A
+
+  git commit -m "release(): $VERSION"
+  echo ".. .. pushing origin master"
+  git push -q origin master
+
+  echo ".. .. adding tag for version $VERSION & pushing orign master"
+  git tag -f $VERSION
+  git push --tags
+}
+
+source $(dirname $0)/utils.inc
---
+++ scripts/update-nuget-versions.ts
@@ -0,0 +1,54 @@
+// updates the ng-office-ui-fabric.nuspec in the ngOfficeUiFabric nuget
+// repo (nuget-ngofficeuifabric) to have the same version & dependencies
+// that are specified in the main source repo's (ng-officeuifabric)
+// package.json file
+//
+// two parameters are expected:
+// --src = location of the cloned source repo (ng-officeuifabric)
+// --pkg = location of the cloned package repo (nuget-ngofficeuifabric)
+
+'use strict';
+
+import {ScriptUtils, ILibraryDependencies} from './ScriptUtils';
+import * as fs from 'fs';
+import * as yargs from 'yargs';
+let xmldom: any = require('xmldom');
+import * as xpath from 'xpath';
+
+// verify required params passed in
+if (!yargs.argv.src || !yargs.argv.pkg) {
+  console.error('must specify the path to \'--src\' & \'--pkg\'');
+  process.exit();
+}
+
+// nuspec file
+let nuspecFile: string = yargs.argv.pkg + '/src/ng-office-ui-fabric.nuspec';
+
+// get library version & dependencies
+let libraryVersion: string = ScriptUtils.getLibraryVersion(yargs.argv.src);
+let deps: ILibraryDependencies = ScriptUtils.getDependencies(yargs.argv.src);
+
+// load XML
+let domParser: DOMParser = new xmldom.DOMParser();
+let packageContents: string = fs.readFileSync(nuspecFile).toString();
+let packageManifest: Document = domParser.parseFromString(packageContents, 'text/xml');
+let query: xpath.SelectFn = xpath.useNamespaces({ nuget: 'http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd' });
+
+// update nuspec
+//  update package version
+let versionNode: Node = query('/nuget:package/nuget:metadata/nuget:version', packageManifest, true);
+versionNode.textContent = libraryVersion;
+//  update anuglar version
+let angularVersionNode: any = query('/nuget:package/nuget:metadata/nuget:dependencies/nuget:dependency[@id=\'AngularJS.Core\']',
+                                    packageManifest,
+                                    true);
+angularVersionNode.setAttribute('version', deps.angularLib);
+//  update fabric version
+let fabricVersionNode: any = query('/nuget:package/nuget:metadata/nuget:dependencies/nuget:dependency[@id=\'OfficeUIFabric\']',
+                                   packageManifest,
+                                   true);
+fabricVersionNode.setAttribute('version', deps.officeUiFabricLib);
+
+// save file changes
+let domSerialzer: XMLSerializer = new xmldom.XMLSerializer();
+fs.writeFileSync(yargs.argv.pkg + '/src/ng-office-ui-fabric.nuspec', domSerialzer.serializeToString(packageManifest));
