5493188401582c5b43e6f2e7c7b38beae6685950
Jerzy Czopek <jerzy.czopek@outlook.com>
2016-03-23 22:34:13 +0100

    feat(pivot): Pivot component
    Adds Pivot directive
    Closes #33

---
+++ src/components/pivot/demo/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var demoApp = angular.module('demoApp', [
+  'officeuifabric.core',
+  'officeuifabric.components.pivot',
+  'officeuifabric.components.dropdown'
+]).controller('pivotController', ['$scope', pivotController]);
+
+function pivotController($scope) {
+  $scope.vm = {
+    selectedSize: "large",
+    selectedType: "tabs",
+    pivots: [
+      {title: "My files"},
+      {title: "Recent"},
+      {title: "Shared with me"}
+    ],
+    selectedPivotTitle: undefined,
+    selectedPivot: undefined,
+    menuOpened: false
+  };
+
+  $scope.openMenu = function(){
+    $scope.vm.menuOpened = !$scope.vm.menuOpened;
+  }
+
+  $scope.$watch('vm.selectedPivotTitle', function(newTitle){
+    $scope.vm.selectedPivot = {title: newTitle};
+  }, true);
+
+  $scope.$watch('vm.selectedPivot', function(newPivot){
+
+    if(newPivot !== undefined) {
+      $scope.vm.selectedPivotTitle = newPivot.title;
+    }
+  }, true);
+
+
+}
