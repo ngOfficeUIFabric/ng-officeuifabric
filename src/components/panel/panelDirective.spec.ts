aa2341d6aecd52d2fa5ff1e9731094e9085c0253
Tobias West <tobias@sharepointdiary.co.uk>
2016-04-11 08:50:55 +0100

    New Panel Directive
    Closes issue #29
    
    Code coverage 100%, vet passes
    
    Update to uifCommandBar to allow for the recalculation of the visible
    items via broadcast i.e. in circumstances where the shown items are
    required to be updated but a window.resize has not fired

---
+++ src/components/panel/panelDirective.spec.ts
@@ -0,0 +1,235 @@
+'use strict';
+
+describe('panel: <uif-panel />', () => {
+  beforeEach(() => {
+    angular.mock.module('officeuifabric.core');
+    angular.mock.module('officeuifabric.components.panel');
+  });
+
+  describe('Basic Tests', () => {
+
+    let panel: JQuery;
+    let $scope: any;
+
+    afterEach(() => {
+        this.$timeoutservice.verifyNoPendingTasks();
+    });
+
+    beforeEach(inject(($rootScope: angular.IRootScopeService, $compile: Function, $timeout: angular.ITimeoutService) => {
+
+      $scope = $rootScope;
+      this.$timeoutservice = $timeout;
+      $scope.isOpen = true;
+
+      panel = $compile(`<uif-panel uif-type="small" uif-is-open="isOpen" uif-show-overlay="true" uif-show-close="true">
+                              <uif-panel-header>Header</uif-panel-header>
+                              <uif-content>
+                              <span class="ms-font-m">Place your content in here!</span>
+                              </uif-content>
+                            </uif-panel>
+                    `)($scope);
+      $scope.$digest();
+      $('body').append(panel);
+      panel = jQuery(panel[0]);
+
+
+    }));
+
+    it('should render correct html', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      expect(panel).toHaveClass('ms-Panel');
+      expect(panel.find('div.ms-Overlay').length).toBe(1);
+      expect(panel.find('div.ms-Panel-main').length).toBe(1);
+      expect(panel.find('div.ms-Panel-commands').length).toBe(1);
+      this.$timeoutservice.flush();
+
+    }));
+
+    it('should test for incorrect open value', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      $scope.isOpen = 'stringvalue';
+      $rootScope.$digest();
+      this.$timeoutservice.flush();
+      // test to ensure code coverage
+      expect(panel).toHaveClass('ms-Panel');
+
+    }));
+
+    it('should allow the panel to be opened', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      $scope.isOpen = true;
+      $rootScope.$digest();
+      this.$timeoutservice.flush();
+      expect(panel).toHaveClass('is-open');
+    }));
+
+    it('should allow the panel to be closed', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+
+      $scope.isOpen = true;
+      $rootScope.$digest();
+      this.$timeoutservice.flush();
+      expect(panel).toHaveClass('is-open');
+
+      $scope.isOpen = false;
+      $rootScope.$digest();
+      this.$timeoutservice.flush();
+      expect(panel).not.toHaveClass('is-open');
+
+    }));
+
+    it('close button should close the panel', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+
+      $scope.isOpen = true;
+      $rootScope.$digest();
+      expect(panel).toHaveClass('is-open');
+
+      // close the panel by pressing the button
+      panel.find('.ms-Panel-closeButton').trigger('click');
+      $rootScope.$digest();
+      this.$timeoutservice.flush();
+      expect($scope.isOpen).toEqual(false);
+    }));
+
+  });
+
+  describe('Command bar is placed correctly', () => {
+
+    let panel: JQuery;
+    let $scope: any;
+
+    beforeEach(inject(($rootScope: angular.IRootScopeService, $compile: Function) => {
+
+      $scope = $rootScope;
+
+      panel = $compile(`
+                            <uif-panel uif-is-open="isOpen" uif-show-overlay="true" uif-show-close="true">
+                              <uif-command-bar uif-search-term="searchValue" placeholder="Search here...">
+                                <uif-command-bar-main uif-show-overflow='true'>
+                                <uif-command-bar-item>
+                                  <uif-icon uif-type="save"></uif-icon>
+                                  <span class='ms-font-s'>Save</span>
+                                </uif-command-bar-item>
+                                <uif-command-bar-item>
+                                  <uif-icon uif-type="x"></uif-icon>
+                                  <span class='ms-font-s'>Cancel</span>
+                                </uif-command-bar-item>
+                                </uif-command-bar-main>
+                              </uif-command-bar>
+                              <uif-panel-header>Header</uif-panel-header>
+                              <uif-content>
+                              <span class="ms-font-m">Place your content in here!</span>
+                              </uif-content>
+                            </uif-panel>
+                    `)($scope);
+      $scope.$digest();
+      $('body').append(panel);
+      panel = jQuery(panel[0]);
+
+
+    }));
+
+    it('should render correct html', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      expect(panel.find('div.ms-CommandBar').length).toBe(1);
+    }));
+
+    it('should allow the panel to be opened & show the commandbar', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+
+      $scope.isOpen = true;
+      $rootScope.$digest();
+      expect(panel).toHaveClass('is-open');
+      expect(panel.find('div.ms-CommandBarItem')[0]).not.toHaveClass('is-hidden');
+    }));
+
+
+    });
+
+  describe('Ensure default is applied when no uif-type is supplied', () => {
+    let panel: JQuery;
+    let $scope: any;
+
+    beforeEach(inject(($rootScope: angular.IRootScopeService, $compile: Function) => {
+
+      $scope = $rootScope;
+
+      panel = $compile(`
+                            <uif-panel uif-is-open="vm.isOpen1" uif-show-overlay="true" uif-show-close="true">
+                              <uif-panel-header>Header</uif-panel-header>
+                              <uif-content>
+                              <span class="ms-font-m">Place your content in here!</span>
+                              </uif-content>
+                            </uif-panel>
+                    `)($scope);
+      $scope.$digest();
+      $('body').append(panel);
+      panel = jQuery(panel[0]);
+
+
+    }));
+
+    it('should render correct html', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      expect(panel).toHaveClass('ms-Panel');
+      expect(panel).toHaveClass('ms-Panel--medium');
+    }));
+
+  });
+
+
+  describe('Left panel renders', () => {
+    let panel: JQuery;
+    let $scope: any;
+
+    beforeEach(inject(($rootScope: angular.IRootScopeService, $compile: Function) => {
+
+      $scope = $rootScope;
+
+      panel = $compile(`
+                            <uif-panel uif-type="left" uif-is-open="vm.isOpen1" uif-show-overlay="true" uif-show-close="true">
+                              <uif-panel-header>Header</uif-panel-header>
+                              <uif-content>
+                              <span class="ms-font-m">Place your content in here!</span>
+                              </uif-content>
+                            </uif-panel>
+                    `)($scope);
+      $scope.$digest();
+      $('body').append(panel);
+      panel = jQuery(panel[0]);
+
+
+    }));
+
+    it('should render correct html', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      expect(panel).toHaveClass('ms-Panel');
+      expect(panel).toHaveClass('ms-Panel--left');
+    }));
+
+  });
+
+  describe('Handles incorrect panel types', () => {
+    let panel: JQuery;
+    let $scope: any;
+
+    beforeEach(inject(($rootScope: angular.IRootScopeService, $compile: Function) => {
+
+      $scope = $rootScope;
+
+      panel = $compile(`
+                            <uif-panel uif-type="invalid" uif-is-open="vm.isOpen1" uif-show-overlay="true" uif-show-close="true">
+                              <uif-panel-header>Header</uif-panel-header>
+                              <uif-content>
+                              <span class="ms-font-m">Place your content in here!</span>
+                              </uif-content>
+                            </uif-panel>
+                    `)($scope);
+      $scope.$digest();
+      $('body').append(panel);
+      panel = jQuery(panel[0]);
+
+
+    }));
+
+    it('should render correct html', inject(($compile: Function, $rootScope: angular.IRootScopeService) => {
+      expect(panel).toHaveClass('ms-Panel');
+      // default to medium
+      expect(panel).toHaveClass('ms-Panel--medium');
+    }));
+
+  });
+
+});
---
+++ src/components/panel/panelDirective.ts
@@ -0,0 +1,230 @@
+'use strict';
+
+import * as ng from 'angular';
+import {PanelTypes} from './panelDirectiveEnum';
+
+/**
+ * @ngdoc directive
+ * @name uifPanel
+ * @module officeuifabric.components.panel
+ *
+ * @restrict E
+ *
+ * @description
+ * `<uif-panel>` is the Panel directive.
+ *
+ * @see {link http://dev.office.com/fabric/components/panel}
+ *
+ * @usage
+ *
+ */
+export class PanelDirective implements ng.IDirective {
+
+  public restrict: string = 'E';
+  public template: string = `<div class="ms-Panel">
+                              <div  class="ms-Overlay"
+                                    ng-click="closePanel()"
+                                    ng-class="uifShowOverlay === true ? \'ms-Overlay--dark\' : \'\';"></div>
+                              <div class="ms-Panel-main">
+                                <div class="ms-Panel-commands">
+                                  <button ng-if="uifShowClose" class='ms-Panel-closeButton' ng-click="closePanel()">
+                                    <uif-icon uif-type='x'></uif-icon>
+                                  </button>
+                                </div>
+                                <div class="ms-Panel-contentInner">
+                                </div>
+                              </div>
+                             </div>`;
+  public transclude: Boolean = true;
+  public replace: boolean = true;
+  public controller: any = PanelController;
+
+  public scope: {} = {
+      uifIsOpen: '=',
+      uifShowClose: '=',
+      uifShowOverlay: '=',
+      uifType: '@'
+    };
+
+  constructor(private $log: ng.ILogService, private $animate: ng.animate.IAnimateService, private $timeout: ng.ITimeoutService) { }
+
+  public static factory(): ng.IDirectiveFactory {
+    const directive: ng.IDirectiveFactory = ( $log: ng.ILogService,
+                                              $animate: ng.animate.IAnimateService,
+                                              $timeout: ng.ITimeoutService) => new PanelDirective($log, $animate, $timeout);
+    directive.$inject = ['$log', '$animate', '$timeout'];
+    return directive;
+
+  }
+
+  public compile(element: ng.IAugmentedJQuery,
+                 attrs: ng.IAttributes,
+                 transclude: ng.ITranscludeFunction): ng.IDirectivePrePost {
+    return {
+      pre: this.preLink
+    };
+  }
+
+  private preLink(scope: IPanelScope,
+                  elem: ng.IAugmentedJQuery,
+                  attrs: ng.IAttributes,
+                  ctrl: PanelController,
+                  transclude: ng.ITranscludeFunction): void {
+
+        transclude((clone: ng.IAugmentedJQuery) => {
+          for (let i: number = 0; i < clone.length; i++) {
+
+            if (angular.element(clone[i]).hasClass('ms-CommandBar')) {
+              angular.element(elem[0].querySelector('div.ms-Panel-commands')).prepend(clone[i]);
+            } else if (scope.uifType === 'left') {
+              angular.element(elem[0].querySelector('div.ms-Panel-main')).append(clone[i]);
+            } else {
+              angular.element(elem[0].querySelector('div.ms-Panel-contentInner')).append(clone[i]);
+            }
+
+          }
+
+        });
+
+        scope.closePanel = () => {
+            scope.uifIsOpen = false;
+        };
+
+      }
+
+}
+
+/**
+ * @ngdoc interface
+ * @name IPanelScope
+ * @module officeuifabric.components.panel
+ *
+ * @description
+ * This is the scope used by the <uif-panel> directive.
+ *
+ * @property {boolean} uifIsOpen    -   The string value in the uifPanelSearch input
+ * @property {string} uifType           -   The placeholder for the search input
+ */
+interface IPanelScope extends ng.IScope {
+  uifIsOpen: boolean;
+  uifType: string;
+  uifShowOverlay: boolean;
+  uifShowClose: boolean;
+  closePanel: () => void;
+}
+
+
+/**
+ * @ngdoc controller
+ * @name PanelController
+ * @module officeuifabric.components.panel
+ *
+ * @description
+ * Controller used for the `<uif-panel>` directive.
+ */
+export class PanelController {
+
+  public static $inject: string[] = ['$scope', '$animate', '$element', '$log', '$timeout'];
+
+  constructor(
+    private $scope: IPanelScope,
+    private $animate: ng.animate.IAnimateService,
+    private $element: ng.IAugmentedJQuery,
+    public $log: ng.ILogService,
+    public $timeout: ng.ITimeoutService) {
+
+      if (!$scope.uifType) {
+        $scope.uifType = 'medium';
+      }
+
+      if (PanelTypes[$scope.uifType] === undefined) {
+          this.$log.error('Error [ngOfficeUiFabric] officeuifabric.components.panel - unsupported panel type:\n' +
+            'the type \'' + $scope.uifType + '\' is not supported by ng-Office UI Fabric as valid type for panels.' +
+            'Supported types can be found under PanelTypes enum here:\n' +
+            'https://github.com/ngOfficeUIFabric/ng-officeuifabric/blob/master/src/components/panel/panel.ts');
+
+          $scope.uifType = 'medium';
+      }
+
+      $element.addClass('ms-Panel--' + $scope.uifType);
+
+      $scope.$watch('uifIsOpen', (newValue: boolean) => {
+
+        if (typeof newValue !== 'boolean' && newValue !== undefined) {
+          this.$log.error('Error [ngOfficeUiFabric] officeuifabric.components.panel - invalid attribute type: \'uif-is-open\'.\n' +
+            'The type \'' + typeof newValue + '\' is not supported as valid type for \'uif-is-open\' attribute for ' +
+            '<uif-panel/>. The valid type is boolean.');
+        }
+
+        if (newValue === true) {
+          $animate.addClass(this.$element, 'ms-Panel-animateIn');
+          $element.addClass('is-open');
+
+          // forces resize of commandbars
+          if ($element[0].querySelector('.ms-CommandBar')) {
+            angular.element($element[0].querySelector('.ms-CommandBar')).scope().$broadcast('uif-command-bar-resize');
+          }
+
+        } else {
+
+          $animate.addClass(this.$element, 'ms-Panel-animateOut');
+          $timeout(
+            () => {
+              $element.removeClass('ms-Panel-animateIn ms-Panel-animateOut');
+              $element.removeClass('is-open');
+            },
+            500);
+        }
+
+      });
+  }
+
+}
+
+/**
+ * @ngdoc directive
+ * @name uifPanelHeader
+ * @module officeuifabric.components.panel
+ *
+ * @restrict E
+ *
+ * @description
+ * `<uif-panel-header>` is the placeholder for content the user includes.
+ *
+ * @see {link http://dev.office.com/fabric/components/panel}
+ *
+ * @usage
+ *
+ */
+export class PanelHeaderDirective implements ng.IDirective {
+
+  public restrict: string = 'E';
+  public template: string = '<p class="ms-Panel-headerText" ng-transclude></div>';
+  public transclude: boolean = true;
+  public replace: boolean = true;
+
+  public scope: {} = {
+    uifHeaderText: '@'
+  };
+
+  public static factory(): ng.IDirectiveFactory {
+    const directive: ng.IDirectiveFactory = () => new PanelHeaderDirective();
+    return directive;
+  }
+
+}
+
+
+/**
+ * @ngdoc module
+ * @name officeuifabric.components.panel
+ *
+ * @description
+ * Flyout Panel
+ *
+ */
+export var module: ng.IModule = ng.module('officeuifabric.components.panel', [
+    'officeuifabric.components'
+  ])
+  .directive('uifPanel', PanelDirective.factory())
+  .directive('uifPanelHeader', PanelHeaderDirective.factory());
