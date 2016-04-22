c643a253ef6bbf91efbc68ca7ea97302b7f765e7
Andi Krüger <solutions@andikrueger.de>
2016-04-22 09:25:26 +0200

    feat(table): add support for fixed table
    Added support for new table type fixed, added to Office UI Fabric 2.4.x.
    
    Closes #308.

---
+++ src/components/table/tableTypeEnum.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Enum for supported input types of the uif-table directive
+ * This enum is intended to be used as a string.
+ *
+ * @readonly
+ * @enum {string}
+ * @usage
+ *
+ * This is used to generate the string that you pass into the `uif-table-type` attribute of the following components:
+ *
+ * let style: string = TableTypeEnum[TableTypeEnum.text];
+ */
+export enum TableTypeEnum {
+  fluid,
+  fixed
+}
