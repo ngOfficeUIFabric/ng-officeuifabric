9a3c4d161f0d9bd826e335fc8a86fdbb4de350e6
Andrew Connell <me@andrewconnell.com>
2016-07-26 06:10:55 -0400

    build(wallaby): added support for Wallaby.js
    Added support for the [Wallaby.js](https://wallabyjs.com) test runner. Involves adding a configuration file & a few extra developer plugins. Wallaby.js is not required... testing can still be done with Karma (and Webpack + Gulp + NPM) just like before.
    
    To use Wallaby in this project, first must install the plugin for your editor / IDE. Then compile all project build related TypeScript: `npm run build:ts`. Now configure Wallaby to use the config file for this project: `wallaby.config.js`.
    
    See the Wallaby.js site for more details: https://wallabyjs.com.

---
+++ wallaby.config.ts
@@ -0,0 +1,73 @@
+import { IWallabyConfig, IWallabyFilePattern, IWallabyEnvironment } from 'wallaby';
+import { BuildConfig } from './build/config/build';
+import { WebPackConfig } from './build/config/webpack';
+
+class WallabyConfig implements IWallabyConfig {
+
+  public files: IWallabyFilePattern[] = [
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/angular/angular.js' },
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/angular-mocks/angular-mocks.js' },
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/jquery/dist/jquery.min.js' },
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/pickadate/lib/picker.js' },
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/pickadate/lib/picker.date.js' },
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/jasmine-jquery/lib/jasmine-jquery.js' },
+    { instrument: false, pattern: 'src/core/jquery.phantomjs.fix.js' },
+    { load: false, pattern: 'src/**/*.ts' },
+    { ignore: true, pattern: 'src/**/*.spec.ts' }
+  ];
+
+  public tests: IWallabyFilePattern[] = [
+    { load: false, pattern: 'src/**/*.spec.ts' }
+  ];
+
+  public compilers: any = {
+    'src/**/*.ts': this.wallaby.compilers.typeScript()
+  };
+
+  public env: IWallabyEnvironment = <IWallabyEnvironment>{
+    params: { runner: '--web-security=false' },
+    runner: require('phantomjs2-ext').path
+  };
+
+  public postprocessor: any;
+
+  /**
+   * Configure testing framework / sandbox environbment just before a test run starts.
+   */
+  public setup: any = function (): void {
+    // required to trigger test loadingr
+    let moduleBundle: string = '__moduleBundler';
+    window[moduleBundle].loadTests();
+  };
+
+  constructor(private wallaby: any) {
+    this.configPostProcessor();
+  }
+
+  /**
+   * Configure the Wallaby postprocessor for webpack.
+   */
+  private configPostProcessor(): void {
+    let webpackConfig: WebPackConfig = new WebPackConfig();
+
+    this.postprocessor = require('wallaby-webpack')({
+      entryPatterns: [
+        'src/core/components.js',
+        'src/core/core.js',
+        'src/**/*.spec.js'
+      ],
+      externals: webpackConfig.externals,
+      // can't use same resolve from webpack config because with regular
+      // webpack, need to load TS then JS, but with Wallaby, need JS then TS
+      resolve: {
+        extensions: ['', '.js', '.ts'],
+        root: webpackConfig.resolve.root
+      }
+    });
+  }
+
+}
+
+module.exports = (wallaby: any) => {
+  return new WallabyConfig(wallaby);
+};
